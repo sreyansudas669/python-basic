bcbf2f3af9fc73564a9119c21e6185dc4d149064
sdasworkplace <sreyansudas669@gmail.com>
2024-02-19 21:38:01 +0530

    using lambda and pass wap to how to pass a function without writting a body

---
+++ lambdaandpass.py
@@ -0,0 +1,12 @@
+# Define a lambda function that simply prints a message
+simple_lambda = lambda: print("This is a lambda function.")
+
+# Define a function that uses the pass statement
+def function_with_pass():
+    pass  # This function does nothing
+
+# Test the lambda function
+simple_lambda()
+
+# Test the function with pass
+function_with_pass()
