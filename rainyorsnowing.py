044d1a7d4903b805543d228957e39fa7ef9a7e3b
sdasworkplace <sreyansudas669@gmail.com>
2024-02-21 10:31:14 +0530

    # PRIME AND EVEN NUMBER

--- rainyorsnowing.py
+++ rainyorsnowing.py
@@ -1,24 +1,24 @@
-#checking weather is raining or snowing if it is true either of its true
-
 def getData(weather):
-    val=True
-    data=""
+    global data
+    val = True
     while val:
-        data=input(f" is it {weather}").lower()
-        val  = checking(data)
+        data = input(f"Is it {weather}? ")
+        val = checking(data)
+    return data
 
-        return data
 
-def checking (data):
-    if data == "Yes" or data == "no":
+def checking(data):
+    if data == "yes" or data == "no":
         return False
     else:
         return True
 
-rain = getData("Raining")
+
+rain = getData("raining")
 snow = getData("snowing")
-print(f"{rain} , {snow}")
 
-
-
-
+# Check if either rain or snow is True
+if rain == "yes" or snow == "yes":
+    print("True")
+else:
+    print("False")
