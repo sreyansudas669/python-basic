68433c827e413feef5e883fb027f9c96ae1c63da
s=! <150243300+sreyansudas669@users.noreply.github.com>
2024-02-23 11:12:31 +0530

    missing data from given dataset

--- H - Watermelon.py
+++ H - Watermelon.py
@@ -1,5 +1,6 @@
-n=int(input("enter the slice"))
-if (n<2 or n%2!=0):
-    print("error")
+weight = int(input("enter the peice"))
+if weight > 2 and weight % 2 ==0:
+    for i in range (2,weight//2+1,2):
+        print("[",i,",",weight-i,"]",end= "")
 else:
-    print(n)
+    print("error")
