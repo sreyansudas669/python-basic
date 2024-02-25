64a6c4dbfc0be33bcac5b1d6a0f52c9fa13937fc
s=! <150243300+sreyansudas669@users.noreply.github.com>
2024-02-25 20:50:06 +0530

    one missing and one sum

---
+++ reverseorder.py
@@ -0,0 +1,6 @@
+name = input()
+print(str(name)[::-1])
+
+
+name = input()
+print("%.2f"%name)
---
+++ sumordiffernce.py
@@ -0,0 +1,7 @@
+#Write a program to take two numbers as input and print their difference if the first number is greater than the second numberother wise print the sum
+n1=int(input("enter the first no :"))
+n2=int(input("enter the second no :"))
+if n1 <= n2:
+    print(n1-n2)
+else  :
+    print(n1+n2)
